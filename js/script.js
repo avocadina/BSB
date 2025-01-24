89d36079809767db42fc97dc5a51fb9e1dd1b93d
leSchb <kurultae@yandex.ru>
2025-01-24 16:03:03 +0300

    feat(main): added news

--- js/script.js
+++ js/script.js
@@ -1,26 +1,72 @@
-// partnership slider
-const container = document.querySelector('.partnership__slider');
-const commonSlide = document.querySelectorAll('.p-slider__item');
+const windowWidth = window.innerWidth
+const isSmall = windowWidth <= 1150
+
+// PARTNERSHIP SLIDER
+const commonSlidesPartnership = document.querySelectorAll('.p-slider__item');
 
 const nextButton = document.getElementById('p-next');
 const prevButton = document.getElementById('p-prev');
 
-let index = 0;
-const countOfSlides = commonSlide.length
+let indexP = 0;
+const countOfSlides = commonSlidesPartnership.length;
 
-function updateSlider() {
-    commonSlide.forEach((slide) => {
-        const translateValue = `calc((-100% - var(--slider-gap)) * ${index})`;
+function updatePartnershipSlider() {
+    commonSlidesPartnership.forEach((slide) => {
+        let translateValue = `calc((-100% - var(--slider-gap)) * ${indexP})`;
         slide.style.transform = `translate(${translateValue}, 0)`;
-        slide.style.transition = 'var(--transition)';
     });
 }
 
-prevButton.addEventListener('click', () => {
-    if (index > 0) index -= 1;
-    updateSlider();
+prevButton?.addEventListener('click', () => {
+    if (indexP > 0) {
+        indexP -= 1;
+        updatePartnershipSlider();
+    }
 })
-nextButton.addEventListener('click', () => {
-    if (index <= 5) index += 1;
-    updateSlider();
+nextButton?.addEventListener('click', () => {
+    if (indexP < (countOfSlides - 6)) {
+        indexP += 1;
+        updatePartnershipSlider();
+    }
 });
+
+// NEWS SLIDER
+const commonSlidesNews = document.querySelectorAll('.n-slider__item');
+
+const nextButtonNews = document.getElementById('n-next');
+const prevButtonNews = document.getElementById('n-prev');
+
+let index = 0;
+const countOfNews = commonSlidesNews.length;
+
+function updateSlider() {
+    commonSlidesNews.forEach((slide) => {
+        let translateValue = `calc((-100% - var(--space-6) ${isSmall ? '- var(--padding-4)' : ''}) * ${index})`;
+        slide.style.transform = `translate(${translateValue}, 0)`;
+    });
+}
+
+prevButtonNews.addEventListener('click', () => {
+    if (index > 0) {
+        index -= 1;
+        updateSlider();
+    }
+});
+nextButtonNews.addEventListener('click', () => {
+    if (index < countOfNews - 3) {
+        index += 1;
+        updateSlider();
+    };
+});
+
+// SHAKING SLIDES
+function shakingSlides(slides) {
+    slides.forEach(slide => {
+        if ( !slide.classList.contains('shake') && !slide.classList.contains('normal') ) {
+            slide.classList.add('shake')
+        };
+        setTimeout(() => {
+            slide.classList.remove('shake')
+        }, 2000);
+    });
+};
